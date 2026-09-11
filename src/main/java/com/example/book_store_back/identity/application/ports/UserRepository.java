39c9ba1d513724706763cb01dde245ce00453d15
Henry17C <henryisrael45@gmail.com>
2026-09-11 07:43:02 -0500

    feat(identity): [application] add email parameter to findByEmail method

--- src/main/java/com/example/book_store_back/identity/application/ports/UserRepository.java
+++ src/main/java/com/example/book_store_back/identity/application/ports/UserRepository.java
@@ -2,12 +2,13 @@ package com.example.book_store_back.identity.application.ports;
 
 import java.util.Optional;
 
+import com.example.book_store_back.identity.domain.Email;
 import com.example.book_store_back.identity.domain.User;
 import com.example.book_store_back.identity.domain.UserId;
 
 public interface UserRepository {
     public void save(User user);
     Optional<User> findById(UserId id);
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(Email email);
 
 }
